878d7bd777c72a8efa0ebb6619f60ae51ae9233d
LAVKESH JAISWAL <141453293+lavkeshjazz34@users.noreply.github.com>
2023-09-09 01:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ var n1=Math.random();
 var n2=n1*6;
 var n3=Math.floor(n2+1);
 var str1="dice"+n3+".png";
-var str2="images/"+str1;
 var img1=document.querySelectorAll("img")[0];
-img1.setAttribute("src",str2);
+img1.setAttribute("src",str1);
 var n4=Math.random();
 var n5=n4*6;
 var n6=Math.floor(n5+1);
 var str3="dice"+n6+".png";
-var str4="images/"+str3;
 var img2=document.querySelectorAll("img")[1];
-img2.setAttribute("src",str4);
+img2.setAttribute("src",str3);
 if(n3>n6){
 
     document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
